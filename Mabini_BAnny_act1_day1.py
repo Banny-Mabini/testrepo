e662527283d31f642adf69ac70b4bc3c44742e14
Banny-Mabini <147434202+Banny-Mabini@users.noreply.github.com>
2024-08-10 22:56:39 +0800

    Add files via upload

---
+++ Mabini_BAnny_act1_day1.py
@@ -0,0 +1,17 @@
+print("banny mabz")
+name ="Banny Mabz"
+age = 31
+
+print(name)
+print(str(age))
+
+print(name + str(age))
+print(name +"\t"+ str(age))
+
+name1 = "Banny Mabini"
+math = 80
+science = 81
+english = 82
+status = "pass"
+
+print (name1 +"\t"+ str(math)+"\t"+ str(science)+"\t"+ str(english)+"\t"+ str(status))
